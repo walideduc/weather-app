601d1242d22f08da59085d3703290270a6171757
walideduc <walideduc@gmail.com>
2016-06-09 22:07:07 +0200

    input weatherItem day 6

--- dev/weather/weather-item.component.ts
+++ dev/weather/weather-item.component.ts
@@ -1,19 +1,25 @@
-import {Component} from 'angular2/core';
+import {Component,Input} from 'angular2/core';
+import {WeatherItem} from './weather-item';
 @Component({
     selector: 'weather-item',
     template: `
     <article class="weather-element">
         <div class="col-1">
-            <h3>New York</h3>
-            <p class="info">Clouds</p>
+            <h3>{{ weatherItem.cityName }}</h3>
+            <p class="info">{{ weatherItem.description }}</p>
         </div>
         <div class="col-2">
-            <span class="temperature">32°C</span>
+            <span class="temperature">{{ weatherItem.temperature }}°C</span>
         </div>
     </article>    
                 `,
-    styleUrls:['src/css/weather-item.css']            
+    styleUrls:['src/css/weather-item.css'],
+    // inputs:['weatherItem : item']            
 })
 export class WeatherItemComponent{
+    @Input('item') weatherItem : WeatherItem
+    constructor(){
+        this.weatherItem = new WeatherItem('Paris','sunny',29);
+    }
     
 } 
--- dev/weather/weather-list.component.ts
+++ dev/weather/weather-list.component.ts
@@ -1,17 +1,21 @@
-import {Component} from 'angular2/core';
+import {Component,OnInit} from 'angular2/core';
 import {WeatherItemComponent} from './weather-item.component';
+import {WeatherItem} from './weather-item'
+import {WEATHERITEMS} from './weather.data';
 
 @Component({
     selector: 'weather-list',
     template: `
     <section class="weather-list">
-        <weather-item> </weather-item>
-        <weather-item> </weather-item>
-        <weather-item> </weather-item>
+        <weather-item *ngFor= "#weatheritem of weatheritems" [item]=weatheritem > </weather-item>
     </section>    
                 `,
     directives: [WeatherItemComponent]
 })
-export class WeatherListComponent{
+export class WeatherListComponent implements OnInit{
+    weatheritems : WeatherItem[];
     
+    ngOnInit():any{
+        this.weatheritems = WEATHERITEMS;
+    }
 }
